1477db846b0fea6010b4455144a55e564b26c90d
듀듀 <iam@hyungdew.com>
2019-05-05 14:28:36 +0900

    Modified footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -8,9 +8,5 @@ export const Footer = () => (
     <a href="https://github.com/moodselah" target="_blank">
       moodselah
     </a>
-    , Built with{' '}
-    <a href="https://github.com/JaeYeopHan/gatsby-starter-bee" target="_blank">
-      Gatsby-starter-bee
-    </a>
   </footer>
 )
