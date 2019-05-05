ac3633688ca71d8e4ba9cd629b3c05a7d12add91
듀듀 <iam@hyungdew.com>
2019-05-05 10:05:42 +0900

    Modified style.

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ import * as Dom from '../utils/dom'
 
 import { HOME_TITLE, CATEGORY_TYPE } from '../constants'
 
-const DEST_POS = 316
+const DEST_POS = 382
 const BASE_LINE = 80
 
 function getDistance(currentPos) {
